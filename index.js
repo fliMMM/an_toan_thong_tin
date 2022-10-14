46ff7d493d192416383aefa96a6e49d27a9acd60
bui huy bach <bachkame@gmail.com>
2022-10-14 18:25:56 +0700

    xong playfair

--- index.js
+++ index.js
@@ -45,11 +45,19 @@ function CaesarDecrypt(plainText, cipherText, key) {
 
 //PlayFair
 function PlayFairEncrypt(plainText, cipherText, key) {
-  plainText = plainText.split("");
   key = key.split("");
+
+  //Tách cặp
+  plainText = plainText.split("");
   for (let i = 0; i < plainText.length; i++) {
     if (plainText[i] === plainText[i - 1]) plainText[i] = "X";
   }
+  if (plainText.length % 2 === 1) plainText.push("X");
+  const newPlainText = [];
+  while (plainText.length) newPlainText.push(plainText.splice(0, 2));
+  plainText = [...newPlainText];
+
+  //Tạo key array
   let keyArray = [];
   for (let i = 0; i < key.length; i++) {
     keyArray.push(key[i]);
@@ -66,30 +74,94 @@ function PlayFairEncrypt(plainText, cipherText, key) {
   while (keyArray.length) newArr.push(keyArray.splice(0, 5));
   keyArray = [...newArr];
 
+  //Đảo key aray
+  const newArrayKey = [];
+  for (let i = 0; i < 5; i++) {
+    const temp = [];
+    for (let k = 0; k < 5; k++) {
+      temp.push(keyArray[k][i]);
+    }
+    newArrayKey.push(temp);
+  }
+
   console.log("keyArray: ", keyArray);
   console.log("plainText:", plainText);
 
-  for(let i = 0 ; i< keyArray.length ; i++){
-    for(let j = 0 ; j< plainText.length; j++){
-      if(keyArray[i].indexOf(plainText[j]) !== -1){
-        const index = keyArray[i].indexOf(plainText[j])
-        if(index + 1 > keyArray[i].length -1 ){
-          plainText[j] = keyArray[i][0];
-        }else{
-          plainText[j] = keyArray[i][index+1]
+  let _index = 0;
+
+  //Hang ngang
+  for (let i = 0; i < keyArray.length; i++) {
+    for (let j = 0; j < plainText.length; j++) {
+      if (
+        keyArray[i].indexOf(plainText[j][0]) !== -1 &&
+        keyArray[i].indexOf(plainText[j][1]) !== -1
+      ) {
+        _index++;
+        for (let u = 0; u < 2; u++) {
+          const index = keyArray[i].indexOf(plainText[j][u]);
+          if (index + 1 > keyArray[i].length - 1) {
+            plainText[j][u] = keyArray[i][0];
+          } else {
+            plainText[j][u] = keyArray[i][index + 1];
+          }
         }
       }
     }
-
-
   }
 
-  const newArrayKey = [];
-  for(let i = 0 ; i < keyArray.length ; i++){
-    const temp = [];
-    
+  //Hang doc
+  for (let i = 0; i < newArrayKey.length; i++) {
+    for (let j = 0; j < plainText.length; j++) {
+      if (
+        newArrayKey[i].indexOf(plainText[j][0]) !== -1 &&
+        newArrayKey[i].indexOf(plainText[j][1]) !== -1
+      ) {
+        _index++;
+        for (let u = 0; u < 2; u++) {
+          const index = newArrayKey[i].indexOf(plainText[j][u]);
+          if (index + 1 > newArrayKey[i].length - 1) {
+            plainText[j][u] = newArrayKey[i][0];
+          } else {
+            plainText[j][u] = newArrayKey[i][index + 1];
+          }
+        }
+      }
+    }
   }
-  console.log("plainText:", plainText);
+
+  // hinh chu nhat
+  const temp = [];
+  for (let i = 0; i < plainText.length; i++) {
+    const _temp = [];
+    for (let j = 0; j < plainText[i].length; j++) {
+      _temp.push(plainText[i][j]);
+    }
+    temp.push(_temp);
+  }
+
+  for (let p = _index; p < plainText.length; p++) {
+    let X1 = null,
+      X2 = null,
+      Y1 = null,
+      Y2 = null;
+    for (let j = 0; j < 5; j++) {
+      if (keyArray[j].indexOf(plainText[p][0]) !== -1) {
+        Y1 = keyArray[j].indexOf(plainText[p][0]);
+        X1 = j;
+        
+      }
+
+      if (keyArray[j].indexOf(plainText[p][1]) !== -1) {
+        Y2 = keyArray[j].indexOf(plainText[p][1]);
+        X2 = j;
+      }
+    }
+    temp[p][1] = keyArray[X2][Y1]
+    temp[p][0] = keyArray[X1][Y2]
+  }
+
+  console.log("keyArray: ", keyArray);
+  console.log("plainText:", temp);
 }
 
 // function BruteForce(plainText) {
