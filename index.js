7a70c462f7c7870912389d1f48f27c8785cdcd6c
bui huy bach <bachkame@gmail.com>
2022-10-24 23:01:23 +0700

    xong des encrypt

--- index.js
+++ index.js
@@ -621,12 +621,13 @@ let PC1 = [
 ];
 PC1 = PC1.map((e) => e - 1);
 
-const hoanViKetThuc = [
-  39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13,
-  53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27,
-  34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25, 32, 0, 40, 8, 48,
-  16, 56, 24,
+let hoanViKetThuc = [
+  40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14,
+  54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28,
+  35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9,
+  49, 17, 57, 25,
 ];
+hoanViKetThuc = hoanViKetThuc.map((e) => e - 1);
 
 let hoanViKhoiTao = [
   58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38,
@@ -652,6 +653,70 @@ let bang_mo_rong_nua_phai = [
 ];
 bang_mo_rong_nua_phai = bang_mo_rong_nua_phai.map((e) => e - 1);
 
+const SBox = [
+  [
+    [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+    [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+    [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+    [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13],
+  ],
+
+  [
+    [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+    [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+    [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+    [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9],
+  ],
+
+  [
+    [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+    [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+    [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+    [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12],
+  ],
+
+  [
+    [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+    [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+    [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+    [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14],
+  ],
+
+  [
+    [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+    [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+    [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+    [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3],
+  ],
+
+  [
+    [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+    [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+    [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+    [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13],
+  ],
+
+  [
+    [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+    [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+    [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+    [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12],
+  ],
+
+  [
+    [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+    [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+    [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+    [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11],
+  ],
+];
+
+let P = [
+  16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32,
+  27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25,
+];
+P = P.map((e) => e - 1);
+
 const convertHexToDec = (text) => {
   switch (text) {
     case "A":
@@ -669,6 +734,33 @@ const convertHexToDec = (text) => {
     default:
       return parseInt(text);
   }
+};
+const convertDecToHex = (text) => {
+  switch (text) {
+    case 10:
+      return "A";
+    case 11:
+      return "B";
+    case 12:
+      return "C";
+    case 13:
+      return "D";
+    case 14:
+      return "E";
+    case 15:
+      return "F";
+    default:
+      return parseInt(text);
+  }
+};
+
+const BinToDec = (BinArray) => {
+  const temp = BinArray.reverse();
+  let result = 0;
+  for (let i = 0; i < temp.length; i++) {
+    result += Math.pow(2, i) * temp[i];
+  }
+  return result;
 };
 
 function genFinalKeyDes(key) {
@@ -689,7 +781,7 @@ function genFinalKeyDes(key) {
     key_64bit.push(first + last);
   }
 
-  console.log("key 64bit:", key_64bit);
+  // console.log("key 64bit:", key_64bit);
 
   const KeyArray_1D_64bit = [];
   for (let i = 0; i < key_64bit.length; i++) {
@@ -698,8 +790,8 @@ function genFinalKeyDes(key) {
     }
   }
 
-  console.log("1D key array 64 bit: ", KeyArray_1D_64bit);
-  console.log("Hoan vi khoi tao: ", PC1);
+  // console.log("1D key array 64 bit: ", KeyArray_1D_64bit);
+  // console.log("Hoan vi khoi tao: ", PC1);
 
   //gen PC1K
   let PC1K = [];
@@ -719,7 +811,7 @@ function genFinalKeyDes(key) {
     );
   }
 
-  console.log("Key sau khi dich trai: ", SHiftLeftKey);
+  // console.log("Key sau khi dich trai: ", SHiftLeftKey);
 
   const finalKey = [];
   for (let i = 0; i < SHiftLeftKey.length; i++) {
@@ -751,7 +843,7 @@ function genPlanTextDes(plainText) {
     plainText_64bit.push(first + last);
   }
 
-  console.log("plainText 64bit:", plainText_64bit);
+  // console.log("plainText 64bit:", plainText_64bit);
 
   const plainTextArray_1D_64bit = [];
   for (let i = 0; i < plainText_64bit.length; i++) {
@@ -766,22 +858,108 @@ function genPlanTextDes(plainText) {
     plainText_after_permute.push(plainTextArray_1D_64bit[hoanViKhoiTao[i]]);
   }
 
-  console.log("PlainText sau khi hoan vi: ", plainText_after_permute);
+  // console.log("PlainText sau khi hoan vi: ", plainText_after_permute);
   return plainText_after_permute;
 }
 
-// AABB09182736CCDD
+// 0F1571C947D9E859 
 function DesEncrypt(plainText, cipherText, key) {
-  plainText = "123456ABCD132536".split("");
-  key = "AABB09182736CCDD".split("");
+  plainText = plainText.split("");
+  key = key.split("");
 
   const finalKey = genFinalKeyDes(key);
   let finalPlainText = genPlanTextDes(plainText);
 
+  console.log("16 key: ", finalKey);
+  console.log("plainText sau khi hoán vị: ", finalPlainText);
+
+  let leftPlainText = finalPlainText.slice(0, 32);
+  let rightPlainText = finalPlainText.slice(32, 64);
   for (let i = 0; i < 16; i++) {
-    const leftPlainText = finalPlainText.slice(0, 32);
-    const rightPlainText = finalPlainText.slice(32, 64);
+    //mo rong ben phai
+    const newRightPlainText = [];
+    for (let i = 0; i < bang_mo_rong_nua_phai.length; i++) {
+      newRightPlainText.push(rightPlainText[bang_mo_rong_nua_phai[i]]);
+    }
+
+    let right_plainText_after_xor = [];
+    for (let j = 0; j < finalKey[i].length; j++) {
+      right_plainText_after_xor.push(
+        XOR(newRightPlainText[j].toString(), finalKey[i][j].toString())
+      );
+    }
+
+    //chia thanh 8 cuc 6 bit
+    const new_right_plainText_after_xor = [];
+    while (right_plainText_after_xor.length)
+      new_right_plainText_after_xor.push(
+        right_plainText_after_xor.splice(0, 6)
+      );
+    right_plainText_after_xor = [...new_right_plainText_after_xor];
+    console.log(right_plainText_after_xor);
+
+    //cho vao sBox;
+    const after_sbox = [];
+    for (let i = 0; i < 8; i++) {
+      const firstValue = right_plainText_after_xor[i][0];
+      const lastValue = right_plainText_after_xor[i][5];
+      const middleValue = right_plainText_after_xor[i].slice(1, 5);
+      const SBoxValue =
+        SBox[i][BinToDec([firstValue, lastValue])][BinToDec(middleValue)];
+      after_sbox.push(SBoxValue);
+    }
+
+    console.log("after_sbox: ", after_sbox);
+    //convert after_sbox to bin
+    for (let i = 0; i < after_sbox.length; i++) {
+      after_sbox[i] = convertToBin(after_sbox[i], 4);
+    }
+    const new_after_sbox = [];
+    for (let i = 0; i < after_sbox.length; i++) {
+      for (let j = 0; j < after_sbox[i].length; j++) {
+        new_after_sbox.push(after_sbox[i][j]);
+      }
+    }
+
+    // hoan vi sbox
+    const sbox_after_permute = [];
+    for (let i = 0; i < P.length; i++) {
+      sbox_after_permute.push(new_after_sbox[P[i]]);
+    }
+    console.log("sau khi hoan vi sbox: ", sbox_after_permute);
+
+    //xor sbox voi nua trai;
+    const sbox_after_shift = [];
+    for (let i = 0; i < 32; i++) {
+      sbox_after_shift.push(
+        XOR(sbox_after_permute[i].toString(), leftPlainText[i].toString())
+      );
+    }
+
+    console.log("sbox_after_shift: ", sbox_after_shift);
+    leftPlainText = rightPlainText;
+    rightPlainText = sbox_after_shift;
   }
+
+  console.log(leftPlainText);
+  console.log(rightPlainText);
+
+  const after_16_round = [...rightPlainText, ...leftPlainText];
+  let new_after_16_round = [];
+  for (let i = 0; i < hoanViKetThuc.length; i++) {
+    new_after_16_round.push(after_16_round[hoanViKetThuc[i]]);
+  }
+
+  const temp = [];
+  while (new_after_16_round.length) temp.push(new_after_16_round.splice(0, 4));
+  new_after_16_round = [...temp];
+
+  for (let i = 0; i < new_after_16_round.length; i++) {
+    new_after_16_round[i] = convertDecToHex(BinToDec(new_after_16_round[i]));
+  }
+
+  cipherTextField.value = new_after_16_round.join("");
+  console.log(new_after_16_round.join(""));
 }
 
 function DesDecrypt(plainText, cipherText, key) {}
@@ -790,13 +968,6 @@ function subKeyGenerate(left, right, count) {
   for (let i = 0; i < count; i++) {
     left.push(left.shift());
     right.push(right.shift());
-
-    //mo rong ben phai
-    const newRight = [];
-    for (let i = 0; i < bang_mo_rong_nua_phai.length; i++) {
-      newRight.push(right[bang_mo_rong_nua_phai[i]]);
-    }
-    console.log(`Ben pha sau khi mo rong lan ${i}: `, newRight );
   }
 
   return [...left, ...right];
